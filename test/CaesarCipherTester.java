48f2e6aa5685f031e745d8464e633006c53478a7
Jorge Luis Arteaga Almaraz <jorgitoaral@gmail.com>
2024-01-19 19:18:56 -0400

    cr: improve constants in testing suite

--- test/CaesarCipherTester.java
+++ test/CaesarCipherTester.java
@@ -17,12 +17,9 @@ public class CaesarCipherTester {
             'e',
             'f'
     };
-    private static final String PLAIN_TEXT_TEST = new String(alphabet);
-    private static final String PLAIN_TEXT_TEST_SHIFTED_RIGHT = PLAIN_TEXT_TEST.substring(1,
-            PLAIN_TEXT_TEST.length()) + PLAIN_TEXT_TEST.charAt(0);
-    private static final String PLAIN_TEXT_TEST_SHIFTED_LEFT = PLAIN_TEXT_TEST.charAt(PLAIN_TEXT_TEST.length() - 1) + PLAIN_TEXT_TEST.substring(
-            0,
-            PLAIN_TEXT_TEST.length() - 1);
+    private static final String PLAIN_TEXT_TEST = "abcdef";
+    private static final String PLAIN_TEXT_TEST_SHIFTED_LEFT = "bcdefa";
+    private static final String PLAIN_TEXT_TEST_SHIFTED_RIGHT = "fabcde";
 
     public static void main(String[] args) {
         Instant startedAt = BaseTester.startTest(CaesarCipherTester.class.getName());
@@ -50,10 +47,10 @@ public class CaesarCipherTester {
         cipher.setOffset(1);
         BaseTester.test("[encryptString] should move all characters one position forward",
                 cipher.encryptString(PLAIN_TEXT_TEST),
-                PLAIN_TEXT_TEST_SHIFTED_RIGHT);
+                PLAIN_TEXT_TEST_SHIFTED_LEFT);
         BaseTester.test("[decryptString] should move all characters one position backward",
                 cipher.decryptString(PLAIN_TEXT_TEST),
-                PLAIN_TEXT_TEST_SHIFTED_LEFT);
+                PLAIN_TEXT_TEST_SHIFTED_RIGHT);
     }
 
     private static void testBorderCasesForOffsets(CaesarCipher cipher) {
@@ -71,33 +68,33 @@ public class CaesarCipherTester {
         BaseTester.test(
                 "[encryptString] should gracefully handle negative offsets (1/2)",
                 cipher.encryptString(PLAIN_TEXT_TEST),
-                PLAIN_TEXT_TEST_SHIFTED_LEFT);
+                PLAIN_TEXT_TEST_SHIFTED_RIGHT);
         cipher.setOffset(-CaesarCipherTester.alphabet.length - 1);
         BaseTester.test(
                 "[encryptString] should gracefully handle negative offsets (2/2)",
                 cipher.encryptString(PLAIN_TEXT_TEST),
-                PLAIN_TEXT_TEST_SHIFTED_LEFT);
+                PLAIN_TEXT_TEST_SHIFTED_RIGHT);
         cipher.setOffset(CaesarCipherTester.alphabet.length + 1);
         BaseTester.test(
                 "[encryptString] should gracefully handle positive offsets greater than the size of the alphabet",
                 cipher.encryptString(PLAIN_TEXT_TEST),
-                PLAIN_TEXT_TEST_SHIFTED_RIGHT);
+                PLAIN_TEXT_TEST_SHIFTED_LEFT);
 
         cipher.setOffset(-1);
         BaseTester.test(
                 "[decryptString] should gracefully handle negative offsets (1/2)",
                 cipher.decryptString(PLAIN_TEXT_TEST),
-                PLAIN_TEXT_TEST_SHIFTED_RIGHT);
+                PLAIN_TEXT_TEST_SHIFTED_LEFT);
         cipher.setOffset(-CaesarCipherTester.alphabet.length - 1);
         BaseTester.test(
                 "[decryptString] should gracefully handle negative offsets (2/2)",
                 cipher.decryptString(PLAIN_TEXT_TEST),
-                PLAIN_TEXT_TEST_SHIFTED_RIGHT);
+                PLAIN_TEXT_TEST_SHIFTED_LEFT);
         cipher.setOffset(CaesarCipherTester.alphabet.length + 1);
         BaseTester.test(
                 "[decryptString] should gracefully handle positive offsets greater than the size of the alphabet",
                 cipher.decryptString(PLAIN_TEXT_TEST),
-                PLAIN_TEXT_TEST_SHIFTED_LEFT);
+                PLAIN_TEXT_TEST_SHIFTED_RIGHT);
     }
 
     private static void testFileTransformations(CaesarCipher cipher) {
@@ -109,11 +106,11 @@ public class CaesarCipherTester {
             String result = Files.readString(test1OutputFile);
             BaseTester.test("[encodeFile] should shift test1-input.txt characters one position forward",
                     result,
-                    PLAIN_TEXT_TEST_SHIFTED_RIGHT);
+                    PLAIN_TEXT_TEST_SHIFTED_LEFT);
         } catch (Exception e) {
             BaseTester.test("[encodeFile] should shift test1-input.txt characters one position forward",
                     e.toString(),
-                    PLAIN_TEXT_TEST_SHIFTED_RIGHT);
+                    PLAIN_TEXT_TEST_SHIFTED_LEFT);
             e.printStackTrace();
         }
         try {
